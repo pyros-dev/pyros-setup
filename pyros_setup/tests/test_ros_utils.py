f0ba43c12ea0a2e726112aa8d7b984bb9676f42d
alexv <asmodehn@gmail.com>
2016-01-04 11:59:24 +0900

    adding finally clause to test to cleanup even if tests fail.

--- pyros_setup/tests/test_ros_utils.py
+++ pyros_setup/tests/test_ros_utils.py
@@ -41,10 +41,12 @@ def test_roscore_started():
         import rospy
 
     master, roscore = pyros_setup.get_master()
-    assert master.is_online()
-    if roscore is not None:
-        roscore.terminate()
-    rospy.signal_shutdown('test_roscore_started done')
+    try:
+        assert master.is_online()
+    finally:  # to make sure we always shutdown everything, even if test fails
+        if roscore is not None:
+            roscore.terminate()
+        rospy.signal_shutdown('test_roscore_started done')
 
 
 def test_roslaunch_started():
@@ -57,17 +59,19 @@ def test_roslaunch_started():
         import roslaunch
 
     master, roscore = pyros_setup.get_master()
-    assert master.is_online()
+    try:
+        assert master.is_online()
 
-    time.sleep(2)  # needed until fix for https://github.com/ros/ros_comm/pull/711 is released
-    launch = roslaunch.scriptapi.ROSLaunch()
-    launch.start()
+        time.sleep(2)  # needed until fix for https://github.com/ros/ros_comm/pull/711 is released
+        launch = roslaunch.scriptapi.ROSLaunch()
+        launch.start()
 
-    assert launch.started
+        assert launch.started
 
-    if roscore is not None:
-        roscore.terminate()
-    rospy.signal_shutdown('test_roslaunch_started done')
+    finally:  # to make sure we always shutdown everything, even if test fails
+        if roscore is not None:
+            roscore.terminate()
+        rospy.signal_shutdown('test_roslaunch_started done')
 
 
 def test_rosnode_started():
@@ -83,27 +87,29 @@ def test_rosnode_started():
         import roslaunch
 
     master, roscore = pyros_setup.get_master()
-    assert master.is_online()
+    try:
+        assert master.is_online()
 
-    time.sleep(2)  # needed until fix for https://github.com/ros/ros_comm/pull/711 is released
-    launch = roslaunch.scriptapi.ROSLaunch()
-    launch.start()
+        time.sleep(2)  # needed until fix for https://github.com/ros/ros_comm/pull/711 is released
+        launch = roslaunch.scriptapi.ROSLaunch()
+        launch.start()
 
-    assert launch.started
+        assert launch.started
 
-    echo_node = roslaunch.core.Node('pyros_test', 'echo.py', name='echo')
-    echo_process = launch.launch(echo_node)
-    assert echo_process.is_alive()
+        echo_node = roslaunch.core.Node('pyros_test', 'echo.py', name='echo')
+        echo_process = launch.launch(echo_node)
+        assert echo_process.is_alive()
 
-    node_api = None
-    with timeout(5) as t:
-        while not t.timed_out and node_api is None:
-            node_api = rosnode.get_api_uri(master, 'echo')  # would be good to find a way to do this without rosnode dependency
-    assert node_api is not None
+        node_api = None
+        with timeout(5) as t:
+            while not t.timed_out and node_api is None:
+                node_api = rosnode.get_api_uri(master, 'echo')  # would be good to find a way to do this without rosnode dependency
+        assert node_api is not None
 
-    if roscore is not None:
-        roscore.terminate()
-    rospy.signal_shutdown('test_rosnode_started done')
+    finally:  # to make sure we always shutdown everything, even if test fails
+        if roscore is not None:
+            roscore.terminate()
+        rospy.signal_shutdown('test_rosnode_started done')
 
 if __name__ == '__main__':
     # forcing nose run from python call
