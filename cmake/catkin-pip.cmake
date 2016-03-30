045f1dd104d11f9912ede90f6e33c296b2e4251e
alexv <asmodehn@gmail.com>
2016-03-30 09:54:23 +0900

    added comments...

--- cmake/catkin-pip.cmake
+++ cmake/catkin-pip.cmake
@@ -2,10 +2,10 @@ macro(catkin_pip_install_requirements)
 
     #TODO : check requirements.txt exists
     message(STATUS "    ... Installing Pip requirements :")
-    message(STATUS "    ...    /usr/bin/pip install -r ${CMAKE_CURRENT_SOURCE_DIR}/requirements.txt -t ${CATKIN_DEVEL_PREFIX}/${CATKING_GLOBAL_PYTHON_DESTINATION}")
+    message(STATUS "    ...    /usr/bin/pip install -r ${CMAKE_CURRENT_SOURCE_DIR}/requirements.txt -t ${CATKIN_DEVEL_PREFIX}/${CATKIN_GLOBAL_PYTHON_DESTINATION}")
 
     execute_process(
-      COMMAND /usr/bin/pip install -r ${CMAKE_CURRENT_SOURCE_DIR}/requirements.txt -t ${CATKIN_DEVEL_PREFIX}/${CATKING_GLOBAL_PYTHON_DESTINATION}
+      COMMAND /usr/bin/pip install -r ${CMAKE_CURRENT_SOURCE_DIR}/requirements.txt -t ${CATKIN_DEVEL_PREFIX}/${CATKIN_GLOBAL_PYTHON_DESTINATION}
       WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
       RESULT_VARIABLE PIP_RESULT
       OUTPUT_VARIABLE PIP_VARIABLE
@@ -14,3 +14,8 @@ macro(catkin_pip_install_requirements)
     message(STATUS "    ... Done.. [${PIP_RESULT}]: ${PIP_VARIABLE}")
 
 endmacro()
+
+
+# TODO :
+# venv for easy pip install : investigate https://github.com/KitwareMedical/TubeTK/blob/master/CMake/TubeTKVirtualEnvSetup.cmake
+# CPACK to deb : investigate https://cmake.org/pipermail/cmake/2011-February/042687.html
