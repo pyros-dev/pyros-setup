998ada7def2936afccf89dd83d8f85d8a1654ed6
alexv <asmodehn@gmail.com>
2016-08-11 12:19:42 +0900

    improved main entry point for self test

--- pyros_setup/__main__.py
+++ pyros_setup/__main__.py
@@ -5,12 +5,15 @@ import sys
 
 
 def main():
-    if len(sys.argv) > 1 and sys.argv[1] == '--pytest':
+    if len(sys.argv) > 1 and  sys.argv[1] == '--pytest':
         import pytest  # import only if needed
-        errno = pytest.main(sys.argv[2:])
+        errno = pytest.main(['--pyargs', 'pyros_setup'] + sys.argv[2:])
         sys.exit(errno)
+    else:
+        print("To validate your pyros_setup installation, use : pyros_setup --pytest [<custom pytest arguments>]")
 
 
 if __name__ == "__main__":
     main()
 
+# TODO : improve self test commands (use click if needed)
