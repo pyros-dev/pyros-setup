aa314964a82fc3c93607eb326192caf1abe7f4c8
alexv <asmodehn@gmail.com>
2016-08-29 20:06:11 +0900

    adding method to generate config file in instance folder.

--- pyros_setup/__main__.py
+++ pyros_setup/__main__.py
@@ -10,15 +10,22 @@ def main():
             import pytest  # import only if needed
             errno = pytest.main(['--pyargs', 'pyros_setup'] + sys.argv[2:])
             sys.exit(errno)
-        if sys.argv[1] == '--version':
+        elif sys.argv[1] == '--version':
             import pyros_setup  # import only if needed
             # following pip version output format
             print("pyros_setup {0} from {1} (python {2})".format(pyros_setup.__version__, pyros_setup.__file__, sys.version[:3]))
             return
-        if sys.argv[1] == '--help':
+        elif sys.argv[1] == '--genconfig':
+            import pyros_setup  # import only if needed
+            pyros_setup.configurable_import().generate_default_config()
+            return
+        elif sys.argv[1] == '--help':
             print("Pyros_setup is a tool to help you manipulate python environment setup.")
             print("It is especially useful with ROS and other environments that rely on system python with PYTHONPATH modifications.")
-            print("Usage: pyros_setup [--pytest | --version | --help]")
+            print("Usage: pyros_setup [--pytest | --version | --genconfig | --help]")
+        else:
+            print("Usage: pyros_setup [--pytest | --version | --genconfig | --help]")
+
     else:
         print("To validate your pyros_setup installation, use : pyros_setup --pytest [<custom pytest arguments>]")
         print("To check your pyros_setup version, use : pyros_setup --version")
